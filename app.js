40788a1cc494355d23709fe31b1c36f9491a9204
Imad Rajwani <imadrajwani@gmail.com>
2018-11-15 14:09:12 +0500

    no node_modules.

---
+++ app.js
@@ -0,0 +1,70 @@
+const firebase = require("firebase")
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const Chatkit = require("@pusher/chatkit-server");
+
+const app = express();
+const {firebaseConfig, CHATKIT_INSTANCE_LOCATOR, CHATKIT_SECRET_KEY} = require('./keys');
+firebase.initializeApp(firebaseConfig);
+
+// const firebaseDBHelloWorld = `${firebase.database().ref().once("value", (snap) => {console.log(snap.val())})}`;
+
+const chatkit = new Chatkit.default({
+  instanceLocator: CHATKIT_INSTANCE_LOCATOR,
+  key: CHATKIT_SECRET_KEY
+});
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
+// app.post("/users", (req, res) => {
+//   const { username } = req.body;
+//   firebase.database().ref().once("value", (snapshot) => {
+//     var d = snapshot.val();
+//     var {name} = d.Users[username].profile;
+//     chatkit
+//     .createUser({
+//       id: username,
+//       name: name
+//     })
+//     .then(() => {
+//       console.log(`User created: ${username}`);
+//       res.sendStatus(201);
+//     })
+//     .catch(err => {
+//       if (err.error === "services/chatkit/user_already_exists") {
+//         console.log(`User already exists: ${username}`);
+//         res.sendStatus(200);
+//       } else {
+//         res.status(err.status).json(err);
+//       }
+//     });
+//     return null
+//   })
+  
+// });
+
+app.post("/authenticate", (req, res) => {
+  const authData = chatkit.authenticate({ userId: req.query.user_id });
+  res.status(authData.status).send(authData.body);
+});
+
+app.get('/', (req, res) => res.send('I exist to fulfill a specific role in the authentication flow for chatkit users. \n Imad Rajwani is my lord and master. \n email: imadrajwani@gmail.com'))
+
+
+// let port = process.env.PORT;
+// if (port == null || port == "") {
+//   port = 3003;
+// }
+
+let port = 3003;
+
+app.listen(port, err => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(`Running on port ${port}`);
+  }
+});
---
+++ keys.js
@@ -0,0 +1,15 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBUkOB1x1F-bsZcGDnxXQI76JbU-4n8vqI",
+    authDomain: "nottmystyle-447aa.firebaseapp.com",
+    databaseURL: "https://nottmystyle-447aa.firebaseio.com",
+    projectId: "nottmystyle-447aa",
+    storageBucket: "nottmystyle-447aa.appspot.com",
+    messagingSenderId: "791527199565"
+  };
+
+const CHATKIT_SECRET_KEY = "9b627f79-3aba-48df-af55-838bbb72222d:Pk9vcGeN/h9UQNGVEv609zhjyiPKtmnd0hlBW2T4Hfw="
+const CHATKIT_INSTANCE_LOCATOR = "v1:us1:7a5d48bb-1cda-4129-88fc-a7339330f5eb";
+
+module.exports.firebaseConfig = firebaseConfig
+module.exports.CHATKIT_SECRET_KEY = CHATKIT_SECRET_KEY
+module.exports.CHATKIT_INSTANCE_LOCATOR = CHATKIT_INSTANCE_LOCATOR
