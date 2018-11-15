bc674f1633456bdecabe6233d8ace4cdbfb7d863
Imad Rajwani <imadrajwani@gmail.com>
2018-11-15 14:18:43 +0500

    no message

--- keys.js
+++ keys.js
@@ -6,7 +6,7 @@ const firebaseConfig = {
     storageBucket: "nottmystyle-447aa.appspot.com",
     messagingSenderId: "791527199565"
   };
-
+ 
 const CHATKIT_SECRET_KEY = "9b627f79-3aba-48df-af55-838bbb72222d:Pk9vcGeN/h9UQNGVEv609zhjyiPKtmnd0hlBW2T4Hfw="
 const CHATKIT_INSTANCE_LOCATOR = "v1:us1:7a5d48bb-1cda-4129-88fc-a7339330f5eb";
 
