58eb866a1d954071a659b0108fefc934063b1a07
Imad Rajwani <imadrajwani@gmail.com>
2019-01-12 19:09:12 +0500

    restoration of index.lock file

--- app.js
+++ app.js
@@ -1,14 +1,37 @@
-const firebase = require("firebase")
+const admin = require("firebase-admin")
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const Chatkit = require("@pusher/chatkit-server");
 
 const app = express();
-const {firebaseConfig, CHATKIT_INSTANCE_LOCATOR, CHATKIT_SECRET_KEY} = require('./keys');
-// firebase.initializeApp(firebaseConfig);
+
+const {firebaseAdminConfig, CHATKIT_INSTANCE_LOCATOR, CHATKIT_SECRET_KEY} = require('./keys');
+admin.initializeApp(firebaseAdminConfig);
 
 // const firebaseDBHelloWorld = `${firebase.database().ref().once("value", (snap) => {console.log(snap.val())})}`;
+// function onlyUnique(value, index, self) { 
+//   return self.indexOf(value) === index;
+// };
+// a and b are javascript Date objects
+function dateDiffInDaysGreaterThanThree(a, b) {
+  if(a) {
+    // Discard the time and time-zone information.
+    const utc1 = Date.UTC(a.getFullYear(), a.getMonth(), a.getDate());
+    const utc2 = Date.UTC(b.getFullYear(), b.getMonth(), b.getDate());
+
+    var diff = Math.floor((utc2 - utc1) / 1000 * 60 * 60 * 24); //divide diff in milliseconds between dates by milliseconds per day
+
+    if(diff>3) {
+      return true
+    } else {
+      return false
+    }
+  }
+  else {
+    return false
+  }
+}
 
 const chatkit = new Chatkit.default({
   instanceLocator: CHATKIT_INSTANCE_LOCATOR,
@@ -51,7 +74,66 @@ app.post("/authenticate", (req, res) => {
   res.status(authData.status).send(authData.body);
 });
 
-app.get('/', (req, res) => res.send('I exist to fulfill a specific role in the authentication flow for chatkit users. \n Imad Rajwani is my lord and master. \n email: imadrajwani@gmail.com'))
+app.get("/clean", (req, res) => {
+  admin.database().ref().once("value", (dataFromReference) => {
+    var products = (dataFromReference.val()).Products;
+    // console.log(Array.isArray(products), typeof products, products.length, products);
+    var cleanedProducts = [];
+
+    //PROCEDURES:
+
+    //1. Remove items that have been sold for more than 3 days;
+
+    // products.forEach((product, index) => {
+    //   if(index == 0) {
+    //     //but what if the product.dateSold property isnt there?
+    //     product.sold && dateDiffInDaysGreaterThanThree(product.dateSold, new Date) ? null : cleanedProducts.push(product)
+    //   }
+    //   else {
+    //     product.key == products[index - 1].key ? null : product.sold && dateDiffInDays(product.dateSold, new Date) > 3 ? null : cleanedProducts.push(product);
+    //   }
+    // })
+
+    //2. Remove duplicate items
+
+    cleanedProducts = products.length == 1 ? 
+      products 
+      : 
+      products.filter( (product, index) => {!index || (product.key != products[index - 1].key) })
+      
+    console.log(cleanedProducts, cleanedProducts.length);
+    var updates = {}
+    updates['/Products/'] = cleanedProducts
+    admin.database().ref().update(updates);
+    res.send(cleanedProducts);
+  })
+  
+});
+
+//Delete comments of all users and all products
+//.....
+
+
+//Add a property to each product of dateSold: ''
+app.get('/addDateSold', (req, res) => {
+  admin.database().ref().once("value", (dataFromReference) => { 
+    var products = (dataFromReference.val()).Products;
+    var newProducts = products.map((product) => {
+      delete product.dateSold;
+      product.text.dateSold = '';
+      return product;
+    })
+    // products = products.forEach( (product) => product['dateSold'] = '')
+    // console.log(newProducts)
+    // console.log(products[2].dateSold)
+    var updates = {}
+    updates['/Products/'] = newProducts
+    admin.database().ref().update(updates);
+  } )
+})
+
+app.get('/', (req, res) => res.send('Hi, I exist to fulfill a specific role in the authentication flow for chatkit users. \n Imad Rajwani is my lord and master. \n email: imadrajwani@gmail.com'))
+
 
 
 let port = process.env.PORT;
--- keys.js
+++ keys.js
@@ -1,15 +1,20 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyBUkOB1x1F-bsZcGDnxXQI76JbU-4n8vqI",
-    authDomain: "nottmystyle-447aa.firebaseapp.com",
-    databaseURL: "https://nottmystyle-447aa.firebaseio.com",
-    projectId: "nottmystyle-447aa",
-    storageBucket: "nottmystyle-447aa.appspot.com",
-    messagingSenderId: "791527199565"
-  };
- 
+// const firebaseConfig = {
+//     apiKey: "AIzaSyBUkOB1x1F-bsZcGDnxXQI76JbU-4n8vqI",
+//     authDomain: "nottmystyle-447aa.firebaseapp.com",
+//     databaseURL: "https://nottmystyle-447aa.firebaseio.com",
+//     projectId: "nottmystyle-447aa",
+//     storageBucket: "nottmystyle-447aa.appspot.com",
+//     messagingSenderId: "791527199565"
+//   };
+const admin = require("firebase-admin")
+var serviceAccount = require('./nottmystyle-447aa-9e63bdff4127.json');
+const firebaseAdminConfig = {
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://nottmystyle-447aa.firebaseio.com'
+}
 const CHATKIT_SECRET_KEY = "9b627f79-3aba-48df-af55-838bbb72222d:Pk9vcGeN/h9UQNGVEv609zhjyiPKtmnd0hlBW2T4Hfw="
 const CHATKIT_INSTANCE_LOCATOR = "v1:us1:7a5d48bb-1cda-4129-88fc-a7339330f5eb";
 
-module.exports.firebaseConfig = firebaseConfig
+module.exports.firebaseAdminConfig = firebaseAdminConfig
 module.exports.CHATKIT_SECRET_KEY = CHATKIT_SECRET_KEY
 module.exports.CHATKIT_INSTANCE_LOCATOR = CHATKIT_INSTANCE_LOCATOR
